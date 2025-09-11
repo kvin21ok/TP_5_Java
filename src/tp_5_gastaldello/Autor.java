338729830afb7f8ef3a3ebd7d4ef92e55426fdaa
kevin Gasta <kevin.gastaldello21@gmail.com>
2025-09-11 00:00:42 -0300

    subo ejercicio 3 de vuelta

---
+++ src/tp_5_gastaldello/Autor.java
@@ -0,0 +1,33 @@
+package tp_5_gastaldello;
+
+/**
+ *
+ * @author Kevin Gastaldello
+ */
+public class Autor {
+    
+    private String nombre;
+    private String nacionalidad;
+    
+    public Autor(){}
+    
+    public Autor(String nombre, String nacionalidad){
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getNacionalidad(){
+        return nacionalidad;
+    }
+    public void setNacionalidad(String nacionalidad){
+        this.nacionalidad = nacionalidad;
+    }
+
+}
---
+++ src/tp_5_gastaldello/Libro.java
@@ -0,0 +1,49 @@
+package tp_5_gastaldello;
+
+/**
+ *
+ * @author Kevin Gastaldello
+ */
+public class Libro {
+    
+    private String titulo;
+    private String isbn;
+    private Autor autor;
+    private Editorial editorial;
+    
+    public Libro(){}
+    
+    public Libro(String titulo, String isbn){
+        this.titulo = titulo;
+        this.isbn = isbn;
+    }
+    
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    
+    public String getIsbn(){
+        return isbn;
+    }
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+    
+    public Autor getAutor(){
+        return autor;
+    }
+    public void setAutor(Autor autor){
+        this.autor = autor;
+    }
+    
+    public Editorial getEditorial(){
+        return editorial;
+    }
+    public void setEditorial(Editorial editorial){
+        this.editorial = editorial;
+    }
+
+}
