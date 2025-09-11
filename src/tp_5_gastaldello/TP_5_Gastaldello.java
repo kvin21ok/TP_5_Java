a49fe6538fafb1534124bfeed4ad2d8ef5c8c26b
kevin Gasta <kevin.gastaldello21@gmail.com>
2025-09-10 23:59:20 -0300

    subo ejercicio 3

--- src/tp_5_gastaldello/TP_5_Gastaldello.java
+++ src/tp_5_gastaldello/TP_5_Gastaldello.java
@@ -8,6 +8,26 @@ public class TP_5_Gastaldello {
 
     public static void main(String[] args) {
         
+        // Crear el Autor
+        Autor autor = new Autor("Gabriel García Márquez", "Colombiana");
+
+        // Crear la Editorial
+        Editorial editorial = new Editorial("Sudamericana", "Buenos Aires, Argentina");
+
+        // Crear el Libro
+        Libro libro = new Libro("Cien años de soledad", "978-3-16-148410-0");
+        libro.setAutor(autor);         // asignar autor (unidireccional)
+        libro.setEditorial(editorial); // asignar editorial (agregación)
+
+        // Imprimir información del Libro
+        System.out.println("=== Información del Libro ===");
+        System.out.println("Título: " + libro.getTitulo());
+        System.out.println("ISBN: " + libro.getIsbn());
+        System.out.println("Autor: " + libro.getAutor().getNombre() +
+                           ", Nacionalidad: " + libro.getAutor().getNacionalidad());
+        System.out.println("Editorial: " + libro.getEditorial().getNombre() +
+                           ", Dirección: " + libro.getEditorial().getDireccion());
+        
 
     }
 
